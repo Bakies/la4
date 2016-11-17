baed998ccf0008e65a7d5c08100faddb9b1f0a7f
Jon Bakies <jon@baki.es>
2016-11-16 21:10:41 -0500

    Wrote the first predicate then gave up

--- la4.pl
+++ la4.pl
@@ -3,5 +3,22 @@
 /********************************************
 ** define your three predicates below here **
 ********************************************/
+/* Using predicates married/2, sibling/2, and male/1, write a rule brotherInLaw/2 to tell if someone is a brother-in-law. A person is your brother in law either if they are a brother of someone you are married to, or if they are male and married to one of your siblings. See the following knowledge base for argument ordering: */
+sibling(jeff, tina). 
+married(jeff, mark).
+male(mark).
+% brotherInLaw(mark, tina). will be true
+
+% I implemented these backwards if youre wondering why its y, x
+% lazy solution was to swap the names
+% Brother of someone you are married to 
+brotherInLaw(X, Y) :- male(Y), (sibling(X, Z); sibling(Z, X)), (married(Y, Z); married(Z, Y)).
+% if they are male and married to one of your siblings.
+brotherInLaw(Y, X) :- male(Y), (married(Y, Z); married(Z, Y)), (sibling(Z, X); sibling(X, Z)).
+
+
+
+/* 
+* Write a uniques/2 rule that finds all of the unique elements in a list. The first argument should be an input list and the second argument is the return value. The query uniques([4, 2, ana, 5, 2, cat, ana], L). should result in L = [4, 5, cat]. */ 
 
 
